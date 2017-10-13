0e15b1ee9ff6d458e5003352e0976d449c3fcaf2
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2017-10-13 11:48:15 +0900

    投稿画面を作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,8 @@ class TweetsController < ApplicationController
 	def index
 		@tweet = Tweet.all
 	end
+
+	def new
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get 'tweets' => 'tweets#index'
+  get 'tweets' => 'tweets#index'  #一覧画面
+  get 'tweets/new' => 'tweets#new'#投稿画面
   end
