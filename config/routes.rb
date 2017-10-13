1688fd3342fbd087d956309ca1d910b407041bb5
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2017-10-13 18:48:35 +0900

    ユーザーページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'tweets#index'           #ルーティング
-  get 'tweets' => 'tweets#index'  #一覧画面
-  get 'tweets/new' => 'tweets#new'#投稿画面
+  root 'tweets#index'           #ルーティングパス
+  get 'tweets' => 'tweets#index'  #投稿一覧画面
+  get 'tweets/new' => 'tweets#new'#新規投稿画面
   post 'tweets' => 'tweets#create' #投稿機能
+  get 'users/:id' => 'users#show' #ユーザーページ
 end
