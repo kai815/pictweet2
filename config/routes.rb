d1d86a5a075c2986cf4d29d607369ed1b6b6766d
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2017-10-13 12:17:06 +0900

    投稿完了画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'tweets' => 'tweets#index'  #一覧画面
   get 'tweets/new' => 'tweets#new'#投稿画面
-  post 'tweet' => 'tweets#create' #投稿機能
+  post 'tweets' => 'tweets#create' #投稿機能
 end
