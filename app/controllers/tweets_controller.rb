8c5a88f22b3872ce99576225e0650c3c7816c0b3
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2017-10-14 12:20:06 +0900

    編集機能の追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -17,6 +17,16 @@ class TweetsController < ApplicationController
       tweet.destroy if tweet.user_id == current_user.id
     end
 
+    def edit
+    	@tweet = Tweet.find(params[:id])
+    end
+
+    def update
+    	tweet = Tweet.find(params[:id])
+    	tweet.update(tweet_params)  if tweet.user_id == current_user.id
+    end
+   
+
 
 	private
 	def tweet_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'tweets' => 'tweets#index'  #投稿一覧画面
   get 'tweets/new' => 'tweets#new'#新規投稿画面
   post 'tweets' => 'tweets#create' #投稿機能
-  delete 'tweets/:id' => 'tweets#destroy'
+  delete 'tweets/:id' => 'tweets#destroy' #削除
   get 'users/:id' => 'users#show' #ユーザーページ
+  get 'tweets/:id/edit' => 'tweets#edit' #編集画面
+  patch 'tweets/:id' => 'tweets#update' #編集機能
 end
